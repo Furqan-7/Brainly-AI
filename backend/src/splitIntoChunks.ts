404c16f2a71189c93566d2b94bb74ac006b0fed9
Admin <furqanfurqan6677@gmail.com>
2026-04-28 21:14:10 +0530

    Add SplitIntoChunks

---
+++ backend/src/splitIntoChunks.ts
@@ -0,0 +1,15 @@
+
+export function splitIntoChunks(Text: string, chunkSize: number = 500, overlap: number = 50) {
+    const words = Text.split(" ");
+    const chunks: string[] = [];
+
+    let i = 0;
+
+    while (i < words.length) {
+        const chunk = words.slice(i, i + chunkSize).join(" ");
+        chunks.push(chunk);
+        i += chunkSize - overlap;
+
+    }
+    return chunks;
+} 
