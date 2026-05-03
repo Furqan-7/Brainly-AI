30f292a339e3c4627bb72265344d8071b00e833e
Admin <furqanfurqan6677@gmail.com>
2026-05-03 21:58:32 +0530

    YoutubeTranscript

--- backend/src/fetchTweet.ts
+++ backend/src/fetchTweet.ts
@@ -3,22 +3,28 @@ import * as cheerio from "cheerio";
 import axios from "axios";
 
 export async function fetchTweet(TweetUrl: string): Promise<string> {
+    console.log("fetchTweet file path: " + TweetUrl);
 
+    try {
+        const nitterUrl = TweetUrl.replace("twitter.com", "nitter.net")
+            .replace("x.com", "nitter.net");
 
-    const nitterUrl = TweetUrl.replace("twitter.com", "nitter.net")
-        .replace("x.com", "nitter.net");
+        const { data } = await axios.get(nitterUrl);
+        const $ = cheerio.load(data);
 
-    const { data } = await axios.get(nitterUrl);
-    const $ = cheerio.load(data);
+        console.log("html data: " + data);
 
-    const tweetText = $(".tweet-content").text().trim();
+        const tweetText = $(".tweet-content").text().trim();
 
-    // for threads, get all tweets
-    const allTweets = $(".tweet-content")
-        .map((_, el) => $(el).text().trim())
-        .get()
-        .join("\n\n");
+        // for threads, get all tweets
+        const allTweets = $(".tweet-content")
+            .map((_, el) => $(el).text().trim())
+            .get()
+            .join("\n\n");
 
-    return allTweets;
-
+        return allTweets;
+    } catch (error) {
+        console.log("error in fetch Tweet" + error);
+        return "";
+    }
 }
--- backend/src/pdfToText.ts
+++ backend/src/pdfToText.ts
@@ -4,7 +4,6 @@ import pdfParse from "pdf-parse"
 export async function GetPdfText(file_path: string): Promise<string> {
     console.log("pdfToText file path:" + file_path);
     const dataBuffer = await fs.readFile(file_path);
-
     try {
         const data = await pdfParse(dataBuffer);
         return data.text;
